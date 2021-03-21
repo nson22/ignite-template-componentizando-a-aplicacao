759dc766bb18bdad6d8181d56fe0081bcb006296
Gglecinilson Silva <glecinilson@gmail.com>
2021-03-21 15:13:52 -0400

    :tada: Desafio Ignite

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,24 @@
+import { Button } from "./Button";
+import { useMovies } from "./hooks/MovieContext";
+
 export function SideBar() {
-  // Complete aqui
+
+  const {genres, selectedGenreId, handleClickButton} = useMovies();
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+      <div className="buttons-container">
+          {genres.map(genre => (
+          <Button
+            key={genre.id}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
